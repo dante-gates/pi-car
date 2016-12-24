f16083ebc7f95e77e067213425592442cc40fa91
Dante Gates <dantegates@hotmail.com>
2016-12-24 17:44:01 +0000

    Initial commit

---
+++ gpio.py
@@ -0,0 +1,31 @@
+import time
+import RPi.GPIO as GPIO
+
+
+# with Channel(11) as channel:
+#     channel.strobe(10, .05)
+
+
+class Channel:
+    def __init__(self, channel, *args, **kwargs):
+        # args, kwargs passed to setup
+        self._channel = channel
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(channel, GPIO.OUT, *args, **kwargs)
+    def __enter__(self):
+        return self
+    def __exit__(self, ex_val, ex_ty, ex_tb):
+        self.output(0)
+        GPIO.cleanup(self._channel)
+    def output(self, state):
+        GPIO.output(self._channel, state)
+    def strobe(self, howlong, pause=0.5):
+        t_lapse = 0
+        state = 0
+        while 1:
+            state = (state + 1) % 2
+            self.output(state)
+            time.sleep(pause)
+            t_lapse += pause
+            if t_lapse > howlong:
+                break
