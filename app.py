1b25af0146ec09e3896f882536e04193e2757b92
Dante Gates <dantegates@hotmail.com>
2017-01-11 17:48:14 -0500

    Arrows working in HTML with key press

--- app.py
+++ app.py
@@ -13,11 +13,14 @@ def root():
 @app.route('/_drive')
 def pilot():
     if has_request_context():
-        movement = request.args.get('d', None)  # None for testing
-        with Channel(11) as ch:
-            ch.strobe(10, 0.05)
+        movement = request.args.get('direction', None)  # None for testing
+        print(movement)
+#        with Channel(11) as ch:
+#            ch.strobe(10, 0.05)
+        return 200
     else:
         return 'foo', 200  # TODO: what to return here?
+
 
 
 if __name__ == '__main__':
