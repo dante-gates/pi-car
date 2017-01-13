b100307eb6be42e48a344c92dabfa7e7340ab256
Dante Gates <dantegates@hotmail.com>
2017-01-13 12:22:53 -0500

    Formatting

--- gpio.py
+++ gpio.py
@@ -2,23 +2,23 @@ import time
 import RPi.GPIO as GPIO
 
 
-# with Channel(11) as channel:
-#     channel.strobe(10, .05)
-
-
 class Channel:
     def __init__(self, channel, *args, **kwargs):
         # args, kwargs passed to setup
         self._channel = channel
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(channel, GPIO.OUT, *args, **kwargs)
+
     def __enter__(self):
         return self
+
     def __exit__(self, ex_val, ex_ty, ex_tb):
         self.output(0)
         GPIO.cleanup(self._channel)
+
     def output(self, state):
         GPIO.output(self._channel, state)
+
     def strobe(self, howlong, pause=0.5):
         t_lapse = 0
         state = 0
